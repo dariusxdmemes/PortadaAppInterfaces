528b138721c4955216de4a44ad86a74bbd368b9c
Darius <dartig@alu.edu.gva.es>
2025-09-24 16:55:45 +0200

    Pantalla New Player terminada

--- app/src/main/java/com/example/portadaapp/NewPlayer.kt
+++ app/src/main/java/com/example/portadaapp/NewPlayer.kt
@@ -2,25 +2,19 @@ package com.example.portadaapp
 
 import android.content.res.Configuration.ORIENTATION_LANDSCAPE
 import android.content.res.Configuration.ORIENTATION_PORTRAIT
-import android.graphics.Paint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -31,15 +25,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 
 @Composable
@@ -47,7 +35,7 @@ fun PantallaNewPlayer(modifier: Modifier = Modifier, navController: NavHostContr
 
     when (LocalConfiguration.current.orientation) {
         ORIENTATION_LANDSCAPE -> {
-            Orientacion_HorizontalNP(modifier, navController)
+            Orientacion_VerticalNP(modifier, navController)
         }
 
         ORIENTATION_PORTRAIT -> {
@@ -67,6 +55,9 @@ fun Orientacion_VerticalNP(modifier: Modifier = Modifier, navController: NavHost
      modifier = modifier
          .fillMaxSize()
          .background(Color.Magenta)
+         .verticalScroll(
+             rememberScrollState()
+         )
  ) {
      Row(
          modifier = Modifier
@@ -79,18 +70,182 @@ fun Orientacion_VerticalNP(modifier: Modifier = Modifier, navController: NavHost
                  .size(50.dp)
          )
 
-         var estadoTextField by remember {
+         var estadoTextFieldNombre by remember {
              mutableStateOf(" ")
          }
 
          TextField(
-             value = estadoTextField,
+             value = estadoTextFieldNombre,
              onValueChange = {
-                 estadoTextField = it
+                 estadoTextFieldNombre = it
              }, label = {
                  Text(text = "Nombre")
              }
          )
+
+     }
+
+     Spacer(
+         modifier = Modifier
+             .size(10.dp)
+     )
+
+     Row(
+         modifier = Modifier
+             .fillMaxWidth(1f)
+     ) {
+
+         Spacer(
+             modifier = Modifier
+                 .size(50.dp)
+         )
+
+         var estadoTextFieldApellidos by remember {
+             mutableStateOf(" ")
+         }
+
+         TextField(
+             value = estadoTextFieldApellidos,
+             onValueChange = {
+                 estadoTextFieldApellidos = it
+             }, label = {
+                 Text(text = "Apellidos")
+             }
+         )
+     }
+
+     Spacer(
+         modifier = Modifier
+             .size(10.dp)
+     )
+
+     Row(
+         modifier = Modifier
+             .fillMaxWidth(1f)
+     ) {
+         Spacer(
+             modifier = Modifier
+                 .size(50.dp)
+         )
+
+         var estadoTextFieldNick by remember {
+             mutableStateOf(" ")
+         }
+
+         TextField(
+             value = estadoTextFieldNick,
+             onValueChange = {
+                 estadoTextFieldNick = it
+             }, label = {
+                 Text(text = "Nickname")
+             }
+         )
+     }
+
+     Spacer(
+         modifier = Modifier
+             .size(10.dp)
+     )
+
+     Row(
+         verticalAlignment = Alignment.CenterVertically,
+         horizontalArrangement = Arrangement.Center,
+         modifier = Modifier
+             .fillMaxWidth(1f)
+     ) {
+         Image(
+             painter = painterResource(id = R.drawable.android),
+             contentDescription = "imagen_android",
+             modifier = Modifier
+                 .size(60.dp)
+         )
+
+         Spacer(
+             modifier = Modifier
+                 .width(15.dp)
+         )
+
+         Button(
+             onClick = {}
+         ) {
+             Text(text = "Change")
+         }
+     }
+
+     Spacer(
+         modifier = Modifier
+             .size(10.dp)
+     )
+
+     Row(modifier = Modifier
+         .fillMaxWidth(1f)
+     ) {
+         Image(
+             painter = painterResource(id = R.drawable.camera),
+             contentDescription = "imagen_camara",
+             modifier = Modifier
+                 .size(50.dp)
+         )
+
+         var estadoTextFieldCamara by remember {
+             mutableStateOf(" ")
+         }
+
+         TextField(
+             value = estadoTextFieldCamara,
+             onValueChange = {
+                 estadoTextFieldCamara = it
+             }, label = {
+                 Text(text = "Teléfono")
+             }
+         )
+     }
+
+     Spacer(
+         modifier = Modifier
+             .size(10.dp)
+     )
+
+     Row(modifier = Modifier
+         .fillMaxWidth(1f)
+     ) {
+         Image(
+             painter = painterResource(id = R.drawable.email),
+             contentDescription = "imagen_email",
+             modifier = Modifier
+                 .size(50.dp)
+         )
+
+         var estadoTextFieldEmail by remember {
+             mutableStateOf(" ")
+         }
+
+         TextField(
+             value = estadoTextFieldEmail,
+             onValueChange = {
+                 estadoTextFieldEmail = it
+             }, label = {
+                 Text(text = "Email")
+             }
+         )
+     }
+
+     Spacer(
+         modifier = Modifier
+             .size(10.dp)
+     )
+
+     Row(
+         verticalAlignment = Alignment.CenterVertically,
+         horizontalArrangement = Arrangement.Center,
+         modifier = Modifier
+             .fillMaxWidth(1f)
+     ) {
+         Button(
+             onClick = {}
+         ) {
+             Text(text = "New player")
+         }
      }
  }
 }
