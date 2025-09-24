b03217a2193c832d14940e9ffd87cec2abf93d96
Darius <dartig@alu.edu.gva.es>
2025-09-24 15:17:05 +0200

    Arreglo de plantilla para hacer la siguiente pantalla

--- app/src/main/java/com/example/portadaapp/NewPlayer.kt
+++ app/src/main/java/com/example/portadaapp/NewPlayer.kt
@@ -1,6 +1,7 @@
 package com.example.portadaapp
 
 import android.content.res.Configuration.ORIENTATION_LANDSCAPE
+import android.content.res.Configuration.ORIENTATION_PORTRAIT
 import android.graphics.Paint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.snapping.SnapPosition
@@ -37,118 +38,24 @@ fun PantallaNewPlayer(modifier: Modifier = Modifier, navController: NavHostContr
     when (LocalConfiguration.current.orientation) {
         ORIENTATION_LANDSCAPE -> {
             Orientacion_HorizontalNP(modifier, navController)
-        } else -> {
-        Orientacion_VerticalNP(modifier, navController)
-    }
+        }
+
+        ORIENTATION_PORTRAIT -> {
+            Orientacion_VerticalNP(modifier, navController)
+        }
+
+        else -> {
+            Orientacion_VerticalNP(modifier, navController)
+        }
     }
 }
 @Composable
 fun Orientacion_VerticalNP(modifier: Modifier = Modifier, navController: NavHostController) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(-10.dp),
-        modifier = Modifier
-            .fillMaxSize()
-            .wrapContentHeight()
-    ) {
-        Text(
-            text = "New player",
-            fontSize = 50.sp,
-            color = Color.Magenta
-        )
 
-        Spacer(modifier = Modifier.size(50.dp))
-
-        val botones = mapOf("Play" to "PantallaPlay",
-            "New Player" to "PantallaNewPlayer",
-            "Preferences" to "PantallaPreferences",
-            "About" to "PantallaAbout")
-
-        botones.forEach { (nombre, ruta) ->
-            Button(
-                onClick = { navController.navigate(ruta) },
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
-                modifier = Modifier
-                    .width(200.dp)
-                    .height(60.dp)
-            ) {
-                Text(text = nombre)
-            }
-        }
-
-//            val botones = listOf("Play", "New Player", "Preferences", "About")
-//            for (i in botones) {
-//                Button(
-//                    onClick = {navController.navigate(i)},
-//                    colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
-//                    modifier = modifier
-//                        .width(200.dp)
-//                        .size(60.dp)
-//                ) {
-//                    Text(text = i)
-//                }
-//            }
-    }
 }
 
 
 @Composable
 fun Orientacion_HorizontalNP(modifier: Modifier = Modifier, navController: NavHostController) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(-10.dp),
-        modifier = modifier.fillMaxSize()
-            .wrapContentHeight(),
-    ) {
-        Text(
-            text = "New player",
-            fontSize = 50.sp,
-            modifier = modifier,
-            color = Color.Magenta
-        )
 
-        Spacer(modifier = Modifier.size(10.dp))
-
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(20.dp)
-        ) {
-            val botonesIzquierda = mapOf(
-                "Play" to "Play",
-                "New Player" to "PantallaNewPlayer"
-            )
-            botonesIzquierda.forEach { (nombre, ruta) ->
-                Button(
-                    onClick = { navController.navigate(ruta) },
-                    colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
-                    modifier = Modifier
-                        .width(180.dp)
-                        .height(60.dp)
-                ) {
-                    Text(text = nombre)
-                }
-            }
-        }
-        Spacer(Modifier.size(50.dp))
-
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(20.dp),
-        ) {
-            val botonesDerecha = mapOf(
-                "Preferences" to "Preferences",
-                "About" to "About"
-            )
-
-            botonesDerecha.forEach { (nombre, ruta) ->
-                Button(
-                    onClick = { navController.navigate(ruta) },
-                    colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
-                    modifier = Modifier
-                        .width(180.dp)
-                        .height(60.dp)
-                ) {
-                    Text(text = nombre)
-                }
-            }
-        }
-    }
 }
