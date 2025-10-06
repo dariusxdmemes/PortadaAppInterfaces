73c87dad2d2096a5e4845501163c6ff4060b2e91
Darius <dartig@alu.edu.gva.es>
2025-10-06 20:54:43 +0200

    Arreglada pantalla, falta terminar

--- app/src/main/java/com/example/portadaapp/PantallaOpciones.kt
+++ app/src/main/java/com/example/portadaapp/PantallaOpciones.kt
@@ -2,6 +2,9 @@ package com.example.portadaapp
 
 import android.content.res.Configuration.ORIENTATION_LANDSCAPE
 import android.content.res.Configuration.ORIENTATION_PORTRAIT
+import android.transition.Slide
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,7 +14,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -20,10 +34,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.navigation.NavHostController
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import kotlin.math.ceil
+import kotlin.math.floor
 
 @Composable
 fun PantallaOpciones(modifier: Modifier = Modifier, navController: NavHostController) {
@@ -51,6 +71,16 @@ fun Orientacion_VerticalPO(modifier: Modifier, navController: NavHostController)
         mutableStateOf("")
     }
 
+    var sliderPosition by remember {
+        mutableStateOf(0f)
+    }
+
+    var sliderState by rememberSaveable {
+        mutableStateOf("No has seleccionado nada!")
+    }
+
+    val context = LocalContext.current
+
     Box(
         modifier
             .fillMaxSize()
@@ -69,12 +99,42 @@ fun Orientacion_VerticalPO(modifier: Modifier, navController: NavHostController)
             crearBotonesPantalla(selectedOption) {
                 selectedOption = it
             }
+
+            Slider(
+                value = sliderPosition,
+                onValueChange = { sliderPosition = it },
+                Modifier.padding(20.dp),
+                colors = SliderDefaults.colors(
+                    thumbColor = MaterialTheme.colorScheme.secondary,
+                    activeTickColor = MaterialTheme.colorScheme.secondary,
+                    inactiveTickColor = MaterialTheme.colorScheme.secondaryContainer,
+                ),
+                steps = 9,
+                valueRange = 0f..10f
+            )
+
+
+
         }
 
         Spacer(
             modifier = Modifier
                 .size(10.dp)
         )
+
+        FloatingActionButton(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(end = 16.dp,
+                    bottom = 16.dp),
+            onClick = {
+                Toast.makeText(context,"Has seleccionado $selectedOption con una puntuacion de ${sliderPosition.toInt()}",
+                    Toast.LENGTH_LONG).show()
+            }
+        ) {
+            Icon(Icons.Filled.Add, "FAB prueba")
+        }
+
     }
 }
 
@@ -99,6 +159,38 @@ fun crearBotonesPantalla(selectedOption: String, onItemSelected: (String) -> Uni
                 text = opcion,
                 modifier = Modifier
                     .padding(top = 12.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun RatingBar(
+    modifier: Modifier,
+    rating: Double = 0.0,
+    stars: Int = 10,
+    starsColor: Color = Color.Yellow
+) {
+
+    val filledStars = floor(rating).toInt()
+    val unfilledStars = (stars - ceil(rating)).toInt()
+    val halfStar = !(rating.rem(1).equals(0.0))
+
+    Row(
+        modifier = modifier
+    ) {
+        repeat(filledStars) {
+            Icon(
+                imageVector = Icons.Outlined.Star,
+                contentDescription = null, tint = starsColor
+            )
+        }
+
+        if (halfStar) {
+            Icon(
+                imageVector = Icons.Outlined.Close,
+                contentDescription = null,
+                tint = starsColor
             )
         }
     }
