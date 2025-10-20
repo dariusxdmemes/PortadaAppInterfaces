7889f3f11cf43da313323e63c5b09f0e6852a4a1
Darius <dartig@alu.edu.gva.es>
2025-10-20 20:33:06 +0200

    Añadida nueva fuente a la carpeta balatro.otf y modificado el texto "Main" a "Play Juegos"

--- app/src/main/java/com/example/portadaapp/PantallaMain.kt
+++ app/src/main/java/com/example/portadaapp/PantallaMain.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
@@ -20,6 +21,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,6 +44,9 @@ import androidx.navigation.NavHostController
 }
     @Composable
     fun Orientacion_Vertical(modifier: Modifier, navController: NavHostController) {
+
+        val fuenteBalatro = FontFamily(Font(R.font.balatro, FontWeight.Normal))
+
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(20.dp),
@@ -48,13 +55,16 @@ import androidx.navigation.NavHostController
                 .background(Color.Magenta)
         ) {
             Text(
-                text = "Main",
+                text = "Play Juegos",
                 fontSize = 50.sp,
-                color = Color.Black
+                fontFamily = fuenteBalatro,
+                color = Color.Black,
+                modifier = Modifier
+                    .padding(top = 20.dp)
             )
 
             Spacer(modifier = Modifier
-                .size(50.dp))
+                .size(30.dp))
 
             val botones = mapOf("Play" to "PantallaPlay",
                 "New Player" to "PantallaNewPlayer",
